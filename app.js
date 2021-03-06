f408c0c54c8facc3f521a747005a1381df659811
Cuber01 <johnmallard631@gmail.com>
2021-03-06 21:58:34 +0100

    now its somewhat usable and flexible thanks to input you can put in config.json

--- app.js
+++ app.js
@@ -1,33 +1,86 @@
-//import marked from 'markedjs'
 
-//'use strict';
 
 let fs = require('fs');
 
-let slash = '/'
 
+let error_slash = 'An error occured while loading the slash or backslash you entered in config.json, make sure its either a / or a \\ (2x backslash)'
+let error_comment_sequence = 'An error occured while loading the comment sequence you entered in config.json, it seems like you didnt enter anything or entered nothing'
+let error_extension = 'An error occured while loading the file extension you entered in config.json, it seems like you didnt enter anything or entered nothing'
+let error_dir = 'An error occured while loading the path you entered in config.json, it seems like you didnt enter anything or entered nothing'
 
 let str_pos = 0
 let txt = ''
 let lines = ''
 
-let files = []
+let raw_config = fs.readFileSync('config.json');
+let table_config = JSON.parse(raw_config);
 
-
-let filename = ''
+let current_dir = ''
+let comment_sequence = ''
 let extension = ''
+let slash = ''
 
-let current_dir = '/home/cubeq/Other/NuclearDirectory'
+
+let files = []
+let filename = ''
+
 let folders_i = -1
 let folders = []
 
 
+
 console.log('Initialised!')
 
-
+ReadConfig()
 
 GetFolders(current_dir)
 
+function ReadConfig() {
+
+    slash = table_config["Slash or backslash"]
+
+    if (slash != '/' && slash != '\\') {
+        console.log(error_slash)
+        process.exit(1)
+    }
+
+    console.log('Slash or backslash: ' + slash)
+
+    comment_sequence = table_config["Comment sequence"]
+
+    if (comment_sequence == undefined || comment_sequence == "EDIT_THIS") {
+        console.log(error_comment_sequence)
+        process.exit(1)
+    }
+
+    console.log('Comment sequence: ' + comment_sequence)
+
+    extension = table_config["File extension"]
+
+    if (extension == undefined || extension == "EDIT_THIS") {
+        console.log(error_extension)
+        process.exit(1)
+    }
+
+    console.log('File extension: ' + extension)
+
+    current_dir = table_config["Path"]
+
+    if (current_dir == undefined || current_dir == "EDIT_THIS") {
+        console.log(error_dir)
+        process.exit(1)
+    }
+    
+    //  FIX THIS
+    if (current_dir[current_dir.length-1] == "/" || current_dir[current_dir.length-1] == "\\") {
+        current_dir[current_dir.length-1] = ""
+        console.log('Directory input corrected')
+    }
+
+    console.log('Directory: ' + current_dir)
+
+
+}
 
 function GetFolders(dir) {
     //console.log(dir)
@@ -52,9 +105,6 @@ function GetFolders(dir) {
         GetFiles(folders[folders_i], 'c')
         GetFolders(folders[folders_i])
     }
-
-    //console.log(folders_i)
-    //GetFiles(folders[folders_i], 'c')
 
 }
 
@@ -87,14 +137,11 @@ function RemoveComments(file, dir) {
 
     lines = txt.split("\n")
 
-    //console.log(path_to_file)
-
-
     for (let i = 0; i < lines.length; i++) {
 
         for (let j = 0; j < lines[i].length; j++) {
 
-            str_pos = lines[i].search("//ET");
+            str_pos = lines[i].search(comment_sequence);
 
             if (str_pos != -1) {
                 lines[i] = lines[i].substring(0, str_pos)
@@ -114,9 +161,8 @@ function RemoveComments(file, dir) {
     fs.writeFileSync(dir + slash + 'copy.txt', lines.join(""));
     //console.log(current_dir.concat('copy.txt'))
 
-    console.log('Comments removed:', filename)
+    console.log('\nComments removed:', filename)
 }
-
 
 
 function GoToDir(dir) {
